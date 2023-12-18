6f26071ccfd8bb6c2c6a265c57869bdbcc0ed1f3
Owner <Owner@192.168.0.7>
2023-12-17 20:10:52 -0700

    First Commit

---
+++ src/personalinfo/personalinfo.java
@@ -0,0 +1,18 @@
+package personalinfo;
+public class personalinfo {
+    public static void main(String[] args) {
+        // Define the variables
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "1234 Maple Street";
+        String city = "Anytown";
+        String zipCode = "12345";
+
+        // Print the information on individual lines
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
